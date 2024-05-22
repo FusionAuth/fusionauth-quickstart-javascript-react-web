16596409e882a6535b5ac536f6a7a7046b3fb35e
Bri Davidson <bri@madebychalk.com>
2024-05-22 11:19:44 -0600

    Remove Unused Anchors from Menu Bar re:32

--- complete-application/src/components/MenuBar.tsx
+++ complete-application/src/components/MenuBar.tsx
@@ -5,22 +5,11 @@ export default function MenuBar() {
   const {isLoggedIn} = useFusionAuth();
 
   return (
-    <div id="menu-bar" className="menu-bar">
-      {
-        isLoggedIn ? (
-          <>
-            <NavLink to="/make-change" className="menu-link">Make Change</NavLink>
-            <NavLink to="/account" className="menu-link">Account</NavLink>
-          </>
-        ) : (
-          <>
-            <button className="menu-link">About</button>
-            <button className="menu-link">Services</button>
-            <button className="menu-link">Products</button>
-            <button className="menu-link" style={{textDecorationLine: "underline"}}>Home</button>
-          </>
-        )
-      }
-    </div>
-  )
+    isLoggedIn ? (
+      <div id="menu-bar" className="menu-bar">
+        <NavLink to="/make-change" className="menu-link">Make Change</NavLink>
+        <NavLink to="/account" className="menu-link">Account</NavLink>
+      </div>
+    ) : null
+  );
 }
